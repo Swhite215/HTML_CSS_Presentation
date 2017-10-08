c786bf288beafb79cf3b9de22edfd1a7b4f8273c
Spencer White <Spencer.White@carat.com>
2017-10-08 10:01:10 -0400

    Updated HTML lesson

---
+++ html_css.js
@@ -0,0 +1,13 @@
+Quicken Loans Interns
+// Mini HTML & CSS Workshop
+1. HTML & CSS
+2. Basic HTML Structure
+  a. Elements
+  b. Important Tags
+  c. Opening a Webpage
+  d. Multi-Page
+3. Basic CSS Structure
+  a. Property: Value
+  b. Different Properties*
+  c. One form of structure i.e. CSS Grid, Flelxbox,
+4.
